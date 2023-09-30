dff32139724749ca1c235cd6b1556a37c036e8b7
DimitryRukhadze <lokiinflames@gmail.com>
2023-09-30 22:32:11 +0300

    Buying house with tests

---
+++ solutions/byuing_house.py
@@ -0,0 +1,71 @@
+class House:
+    def __init__(self, area: int, price_per_sm: float):
+        self.__area = area
+        self.__price = price_per_sm  # цена за кв.метр
+
+    @property
+    def area(self):
+        return self.__area
+
+    @property
+    def final_price(self) -> float:
+        """
+        Возвращает полную цену дома
+        """
+        return self.__area * self.__price
+
+
+class Human:
+    def __init__(self, name, start_money=0):
+        self.name = name
+        self.__money = start_money
+        self.__house = None  # Изначально у человека нет дома
+
+    @property
+    def money(self):
+        return self.__money
+
+    @property
+    def have_house(self) -> bool:
+        """
+        :return: True - если есть дом, False - если нет
+        """
+        if not self.__house:
+            return False
+        return True
+
+    def info(self) -> str:
+        """
+        возвращает строку формата: "<имя> | на счету: <сумма> | дом: <есть/нет>"
+        """
+        ...
+
+    def earn_money(self, income: int) -> None:
+        """
+        Увеличивает текущее кол-во денег на income
+        """
+        self.__money += income
+
+    def spend_money(self, spend: int) -> None:
+        self.__money -= spend
+
+    def buy_house(self, house: House) -> bool:
+        """
+        Покупка дома
+        Если денег для покупки достаточно:
+            в self.__house сохраняем объект покупаемого дома
+            тратим деньги на покупку
+            возвращаем True
+        Если денег недостаточно:
+            возвращаем False
+        Примечание: Что происходит со старым домом при покупке нового? Он просто исчезает(магия).
+        """
+        house_price = house.final_price
+
+        if house_price > self.money:
+            return False
+
+        self.spend_money(house_price)
+        self.__house = house
+
+        return True
