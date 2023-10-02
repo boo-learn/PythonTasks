63575c68f90ece90a3034513694393df162908e0
DimitryRukhadze <lokiinflames@gmail.com>
2023-10-02 10:06:15 +0300

    Car ride with tests

--- solutions/car.py
+++ solutions/car.py
@@ -3,6 +3,7 @@ class Car():
         self.__capacity = capacity
         self.__gas_per_km = gas_per_km
         self.__gas = gas
+        self.milage = 0
 
     def fill(self, liters: int) -> str:
 
@@ -13,3 +14,16 @@ class Car():
 
         self.__gas += liters
         return f'Has {self.__gas} liters out of {self.__capacity}'
+
+    def ride(self, milage: int) -> None:
+        max_distance = int(self.__gas / self.__gas_per_km)
+        if milage > max_distance:
+            self.__gas == 0
+            km_passed = max_distance
+            self.milage += max_distance
+
+            return f'Out of gas. {milage - km_passed}km left to go.'
+
+        self.__gas -= self.__gas_per_km * milage
+        self.milage += milage
+        return 'Trip finished'
