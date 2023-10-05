3fe11d19eb25327e8c4b0896e2906184236b86bc
DimitryRukhadze <lokiinflames@gmail.com>
2023-10-05 16:26:26 +0300

    __add__ with tests

--- solutions/money.py
+++ solutions/money.py
@@ -12,3 +12,16 @@ class Money:
     def __str__(self):
         rubs_from_cents, cents_left = self.__min_cents()
         return f'{rubs_from_cents + self.__rub}руб {cents_left}коп'
+
+    def __add__(self, other):
+        rub = self.__rub + other.__rub
+        cents = self.__cents + other.__cents
+        return Money(rub, cents)
+
+# Создаем две денежные суммы
+money_sum1 = Money(20, 60)
+money_sum2 = Money(10, 45)
+
+# Складываем суммы
+money_result = money_sum1 + money_sum2
+print(money_result)  # 31руб 5коп
--- tests/test_money.py
+++ tests/test_money.py
@@ -12,11 +12,31 @@ from solutions.money import Money
         (Money(10, 45), '10руб 45коп'),
         (Money(10, 101), '11руб 1коп'),
         (Money(10, 205), '12руб 5коп'),
-        (Money(10, 0), '10руб 0коп')
+        (Money(10, 0), '10руб 0коп'),
+        (Money(10, 99), '10руб 99коп')
     ]
 )
 def test_str_money(money_given, expected):
     out = StringIO()
     sys.stdout = out
     print(money_given)
+    assert out.getvalue().strip() == expected
+
+
+@pytest.mark.parametrize(
+    'first_money,second_money,expected',
+    [
+        (Money(10, 45), Money(10, 55), '21руб 0коп'),
+        (Money(10, 101), Money(1, 200), '14руб 1коп'),
+        (Money(0, 205), Money(0, 0), '2руб 5коп'),
+        (Money(0, 0), Money(0, 205), '2руб 5коп'),
+        (Money(10, 99), Money(10, 99), '21руб 98коп')
+    ]
+)
+def test_add_money(first_money, second_money, expected):
+    summ = first_money + second_money
+    assert isinstance(summ, Money)
+    out = StringIO()
+    sys.stdout = out
+    print(summ)
     assert out.getvalue().strip() == expected
