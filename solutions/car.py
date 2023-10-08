d9fa2a2f342b13b16fa0fbfdd3df81638c6fefe7
DimitryRukhadze <lokiinflames@gmail.com>
2023-10-08 20:22:39 +0300

    Refactored car creation + tests

--- solutions/car.py
+++ solutions/car.py
@@ -1,25 +1,27 @@
 class Car:
-    def __init__(self, capacity: int, gas_per_km: int, gas=0):
-        self.__capacity = capacity
-        self.__gas_per_km = gas_per_km
-        self.__gas = gas
+
+    def __init__(self, capacity: int, gas_per_km: int, gas_to_fill=0):
+        self.capacity = capacity
+        self.gas_per_km = gas_per_km
+        self.gas = 0
+        self.fill(gas_to_fill)
         self.milage = 0
 
     def fill(self, liters: int) -> None:
 
-        if liters > self.__capacity - self.__gas:
-            extra_liters = liters - (self.__capacity - self.__gas)
-            self.__gas = self.__capacity
+        if liters > self.capacity - self.gas:
+            extra_liters = liters - (self.capacity - self.gas)
+            self.gas = self.capacity
             print(f'Tank is full. You have {extra_liters} extra liters')
-
-        self.__gas += liters
+        else:
+            self.gas += liters
 
     def ride(self, distance: int) -> None:
-        max_distance = int(self.__gas / self.__gas_per_km)
+        max_distance = int(self.gas / self.gas_per_km)
         if distance > max_distance:
-            self.__gas = 0
+            self.gas = 0
             self.milage += max_distance
             print(f'Out of gas. {distance - max_distance}km left to go.')
 
-        self.__gas -= self.__gas_per_km * distance
+        self.gas -= self.gas_per_km * distance
         self.milage += distance
--- tests/test_car.py
+++ tests/test_car.py
@@ -3,14 +3,16 @@ import pytest
 from solutions.car import Car
 
 
-@pytest.fixture
-def create_car():
-    return Car(500, 25)
-
-
-def test_car_creation(create_car):
-    car = create_car
-    assert isinstance(car, Car)
+@pytest.mark.parametrize(
+    'car,gas_in_tank',
+    [
+        (Car(400, 25), 0),
+        (Car(600, 25, gas_to_fill=10), 10),
+        (Car(500, 25, gas_to_fill=600), 500)
+    ]
+)
+def test_car_creation(car, gas_in_tank):
+    assert car.gas == gas_in_tank
 
 
 def test_car_fill(create_car):
@@ -21,6 +23,7 @@ def test_car_fill(create_car):
 def test_car_ride(create_car):
     car = create_car
     car.fill(500)
+    assert car.ride(10)
     assert car.milage == 10
-    assert car.ride(100) == 'Out of gas. 90km left to go.'
+    assert str(car.ride(100)) == 'Out of gas. 90km left to go.'
     assert car.milage == 20
