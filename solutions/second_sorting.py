aef11c8a2156641427b2786f1d5f6ecb36ebebd9
DimitryRukhadze <lokiinflames@gmail.com>
2023-10-16 09:09:26 +0300

    third sorting solved

--- solutions/second_sorting.py
+++ solutions/second_sorting.py
@@ -7,8 +7,15 @@ def price_sort(prices: list) -> list:
             prices[curr_index], prices[curr_index + 1] = prices[curr_index + 1], prices[curr_index]
         curr_index += 2
 
-    if len(prices) % 2 and len(prices) > 1:
+    if len(prices) % 2 > 0 and len(prices) > 1:
         if prices[-2] > prices[-1]:
             prices[-2], prices[-1] = prices[-1], prices[-2]
 
     return prices
+
+# prices = input().split()
+# prices = [int(i) for i in prices]
+# sorted_prices = price_sort(prices)
+# max_paycheck = sum([sorted_prices[i] for i in range(len(sorted_prices)) if i % 2 == 0])
+# print(max_paycheck)
+
