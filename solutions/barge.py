da1575129dd14b3b81b6e7b5aa66bc813e7cb602
DimitryRukhadze <lokiinflames@gmail.com>
2023-10-19 12:10:36 +0300

    code/tests fixes

--- solutions/barge.py
+++ solutions/barge.py
@@ -5,6 +5,7 @@ class Mistakes(Enum):
     container_empty = 1
     container_full = 2
     barge_not_empty = 3
+    wrong_barrel_type = 4
 
 
 class Barge:
@@ -12,20 +13,24 @@ class Barge:
         self.__containers = {i: [] for i in range(container_quantity)}
         self.__mistakes = []
 
+    def __get_mistakes(self) -> list[Mistakes]:
+        return self.__mistakes
+
     def add_barrel(self, container_num: int, barrel_type: str) -> None:
         container = self.__containers[container_num]
-        if len(container) > 10:
+        if len(container) > 9:
             self.__mistakes.append(Mistakes.container_full)
         else:
             container.append(barrel_type)
 
-    def remove_barrel(self, container_num: int) -> str:
+    def remove_barrel(self, container_num: int, barrel_type: str) -> None:
         container = self.__containers[container_num]
         if not container:
             self.__mistakes.append(Mistakes.container_empty)
         else:
-            barrel_type = container.pop()
-            return barrel_type
+            removed_barrel_type = container.pop()
+            if removed_barrel_type != barrel_type:
+                self.__mistakes.append(Mistakes.wrong_barrel_type)
 
     def check_empty(self):
         all_containers = self.__containers.values()
@@ -35,5 +40,4 @@ class Barge:
                 break
 
     def check_mistakes(self):
-        if self.__mistakes:
-            print(self.__mistakes)
+        return bool(self.__mistakes)
