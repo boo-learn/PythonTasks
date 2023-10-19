797cfb0a44c9a80e686c58b7446fdb689fd8ef61
DimitryRukhadze <lokiinflames@gmail.com>
2023-10-19 11:29:49 +0300

    Made attributes private

--- solutions/barge.py
+++ solutions/barge.py
@@ -1,29 +1,29 @@
 class Barge:
     def __init__(self, container_quantity: int):
-        self.containers = {i: [] for i in range(container_quantity)}
-        self.mistakes = []
+        self.__containers = {i: [] for i in range(container_quantity)}
+        self.__mistakes = []
 
     def add_barrel(self, container: int, barrel: str) -> None:
-        container = self.containers[container]
+        container = self.__containers[container]
         if len(container) > 10:
-            self.mistakes.append('Container full')
+            self.__mistakes.append('Container full')
         else:
             container.append(barrel)
 
     def remove_barrel(self, container: int) -> str:
-        container = self.containers[container]
+        container = self.__containers[container]
         if not container:
-            self.mistakes.append('Container empty')
+            self.__mistakes.append('Container empty')
         else:
             barrel = container.pop()
             return barrel
 
     def check_empty(self):
-        all_containers = self.containers.values()
+        all_containers = self.__containers.values()
         for container in all_containers:
             if container:
-                self.mistakes.append('Barge is still loaded')
+                self.__mistakes.append('Barge is still loaded')
 
     def check_mistakes(self):
-        if self.mistakes:
-            print(self.mistakes)
+        if self.__mistakes:
+            print(self.__mistakes)
