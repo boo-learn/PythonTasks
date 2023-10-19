44f17b6f8592bb5370cfc882c59ba67c0459120e
DimitryRukhadze <lokiinflames@gmail.com>
2023-10-19 11:45:10 +0300

    Refactored mistakes

--- solutions/barge.py
+++ solutions/barge.py
@@ -1,3 +1,12 @@
+from enum import Enum
+
+
+class Mistakes(Enum):
+    container_empty = 1
+    container_full = 2
+    barge_not_empty = 3
+
+
 class Barge:
     def __init__(self, container_quantity: int):
         self.__containers = {i: [] for i in range(container_quantity)}
@@ -6,14 +15,14 @@ class Barge:
     def add_barrel(self, container_num: int, barrel_type: str) -> None:
         container = self.__containers[container_num]
         if len(container) > 10:
-            self.__mistakes.append('Container full')
+            self.__mistakes.append(Mistakes.container_full)
         else:
             container.append(barrel_type)
 
     def remove_barrel(self, container_num: int) -> str:
         container = self.__containers[container_num]
         if not container:
-            self.__mistakes.append('Container empty')
+            self.__mistakes.append(Mistakes.container_empty)
         else:
             barrel_type = container.pop()
             return barrel_type
@@ -22,7 +31,7 @@ class Barge:
         all_containers = self.__containers.values()
         for container in all_containers:
             if container:
-                self.__mistakes.append('Barge is still loaded')
+                self.__mistakes.append(Mistakes.barge_not_empty)
                 break
 
     def check_mistakes(self):
