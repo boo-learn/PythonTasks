801851f3f95073d430fc032b253e3eaa844eaa3f
DimitryRukhadze <lokiinflames@gmail.com>
2023-10-19 11:34:20 +0300

    refactored var names

--- solutions/barge.py
+++ solutions/barge.py
@@ -3,26 +3,27 @@ class Barge:
         self.__containers = {i: [] for i in range(container_quantity)}
         self.__mistakes = []
 
-    def add_barrel(self, container: int, barrel: str) -> None:
-        container = self.__containers[container]
+    def add_barrel(self, container_num: int, barrel_type: str) -> None:
+        container = self.__containers[container_num]
         if len(container) > 10:
             self.__mistakes.append('Container full')
         else:
-            container.append(barrel)
+            container.append(barrel_type)
 
-    def remove_barrel(self, container: int) -> str:
-        container = self.__containers[container]
+    def remove_barrel(self, container_num: int) -> str:
+        container = self.__containers[container_num]
         if not container:
             self.__mistakes.append('Container empty')
         else:
-            barrel = container.pop()
-            return barrel
+            barrel_type = container.pop()
+            return barrel_type
 
     def check_empty(self):
         all_containers = self.__containers.values()
         for container in all_containers:
             if container:
                 self.__mistakes.append('Barge is still loaded')
+                break
 
     def check_mistakes(self):
         if self.__mistakes:
